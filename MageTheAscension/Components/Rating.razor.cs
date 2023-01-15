0e8749adf9f01c392abdabe9ff99878fdfb62bf2
Willian Gruber <willian.gruber@hotmail.com>
2023-01-15 03:26:00 -0300

    sheet update

---
+++ MageTheAscension/Components/Rating.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MageTheAscension.Components
+{
+    public partial class Rating : ComponentBase
+    {
+        [Parameter] public string? Label { get; set; }
+        [Parameter] public string? Text { get => _text; set { if (value == _text) return; _text = value; TextChanged.InvokeAsync(value); } }
+        [Parameter] public EventCallback<string> TextChanged { get; set; }
+        [Parameter] public int Value { get => _value; set { if (value == _value) return; _value = value; ValueChanged.InvokeAsync(value); } }
+        [Parameter] public EventCallback<int> ValueChanged { get; set; }
+
+        private int _value;
+        private string? _text;
+    }
+}
---
+++ MageTheAscension/Pages/Index.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MageTheAscension.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        public MageTheAscensionM20Sheet Sheet { get; set; } = new MageTheAscensionM20Sheet();
+
+
+    }
+}
